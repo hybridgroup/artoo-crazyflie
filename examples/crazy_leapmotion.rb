3e21f02fd9e143a936fa4f5c74ff4ebd4c3baf99
deadprogram <ron@hybridgroup.com>
2013-10-09 16:57:19 -0700

    Cleanup example a bit

--- examples/crazy_leapmotion.rb
+++ examples/crazy_leapmotion.rb
@@ -1,19 +1,25 @@
 require 'artoo'
 
 connection :crazyflie, :adaptor => :crazyflie, :supports_hover => true
-device :drone, :driver => :crazyflie, :connection => :crazyflie, :interval => 0.01
+device :drone, :driver => :crazyflie, :connection => :crazyflie
 
 connection :leap, :adaptor => :leapmotion, :port => '127.0.0.1:6437'
 device :leap, :connection => :leap, :driver => :leapmotion
 
 work do
   on leap, :hand => :wave
-  every(0.1) do
+  every(0.05) do
+    calculate_power
     handle_thrust
   end
 end
 
-def handle_thrust
+def wave sender, hand
+  return unless hand
+  @thrust =  hand.palm_z.from_scale(100..800).to_scale(0..60000)
+end
+
+def calculate_power
   @degrade = 700
   @power = 0 if @power.nil?
   @thrust = 0 if @thrust.nil?
@@ -25,10 +31,8 @@ def handle_thrust
   else
     @power = @power - @degrade
   end
-  drone.power(@power)
 end
 
-def wave sender, hand
-  return unless hand
-  @thrust =  hand.palm_z.from_scale(100..800).to_scale(0..60000)
+def handle_thrust
+  drone.power(@power)
 end
